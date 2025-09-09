98d0acfc338c35b482b8ebff09fc4a10339dad16
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-09-09 23:32:18 +0530

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',   
-  withCredentials: false,                 
+  baseURL: process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000/api/'
+    : 'https://online-course-tracker-backend.onrender.com/api/',
+  withCredentials: false,
 });
-
-export default api;
